33433091893a73b6354f01fdb7a37d6e1e6547e1
Mittal <niteesh.mittal@vodafone.com>
2018-05-04 16:10:09 +0100

    varaibles and data-types

--- python/Hello.py
+++ python/Hello.py
@@ -1,3 +1,13 @@
-print("Hello from python notebook.")
+print("Hello from python notebook.");
 
-print("Hello again.")
+a=1;
+b=c=40.23;
+d='nits';
+e,f=22,'hi';
+#print a;
+print(a,b,c,d,e,f);
+print(d[2]);
+print(d[1:3]);
+print(d[2:]);
+print(d[:2]);
+print(d+" tests");
