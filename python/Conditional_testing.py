e407206830e099b2f72a27eda1a37d0c728da755
Mittal <niteesh.mittal@vodafone.com>
2018-05-08 16:18:56 +0100

    Loop and conditions

---
+++ python/Conditional_testing.py
@@ -0,0 +1,15 @@
+print("This program is a conditional test.");
+
+a=5;
+
+if (a > 1) | (a<10):
+	print('condition is true');
+
+if a>10:
+	print('Condition is true');
+elif a>6:
+	print('Second condition is true');
+else:
+	print('False condition');
+
+print('Program Over');
---
+++ python/Loop_testing.py
@@ -0,0 +1,18 @@
+print("This program is a Loop test.");
+
+print("While Loop")
+count=0;
+while (count<5):
+	print(count,' is less than 5');
+	count+=1;
+else:
+	print(count,' is not less than 5');
+
+print('For Loop')
+
+for num in range(1,11):
+	print('Number is: ',num);
+else:
+	print('Loop end. Now the number is: ',num);
+
+print('Program Over');
